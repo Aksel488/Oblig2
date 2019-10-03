7c2bf1c3c6bb27518fda3e28709dd88d2342c8f9
Aksel488 <aksel.susegg@gmail.com>
2019-10-03 19:39:57 +0200

    test

--- src/DobbeltLenketListe.java
+++ src/DobbeltLenketListe.java
@@ -18,7 +18,7 @@ public class DobbeltLenketListe<T> implements Liste<T> {
 
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello");
     }
 
     /**
